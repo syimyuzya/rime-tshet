4513f10ac825850965136188f9fad78fe26898e6
syimyuzya <syimyuzya@gmail.com>
2022-03-24 17:42:31 +0800

    Support 凡韻非脣音

--- scripts/generate_map.js
+++ scripts/generate_map.js
@@ -21,6 +21,12 @@ function* 生成音韻地位() {
         '並一歌上',
         '生合三支上',
         '幫A脂平',
+        // 凡韻非幫組
+        '溪凡上',
+        '徹凡上',
+        '溪凡入',
+        '徹凡入',
+        '孃凡入',
     ].map((描述) => Qieyun.音韻地位.from描述(描述));
 }
 
@@ -33,7 +39,14 @@ for (const 音韻地位 of 生成音韻地位()) {
     const { 最簡描述 } = 音韻地位;
 
     const kyonh_ = kyonh_override[最簡描述] || kyonh(音韻地位);
-    const tshet_ = tshet(音韻地位);
+    // HACK 目前（20220316）版 tshet 需特別處理凡韻非脣音
+    // （main 分支已修正，將包含於下版）
+    let tshet_;
+    if (音韻地位.屬於('凡韻 且 非 脣音')) {
+        tshet_ = tshet(Qieyun.音韻地位.from描述(最簡描述.replace('凡', '嚴')));
+    } else {
+        tshet_ = tshet(音韻地位);
+    }
 
     kyonh_map.push(最簡描述 + '\t' + kyonh_);
     tshet_map.push(最簡描述 + '\t' + tshet_);
